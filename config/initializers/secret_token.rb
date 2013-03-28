8451ba24df9355c10523d41ff1c57a6d98615124
Peter Lejeck <peter.lejeck@gmail.com>
2013-03-28 13:32:47 -0700

    Initial commit of Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmoteCloud::Application.config.secret_key_base = '730b26a7c2df23718406729e83d88775479f6ad94af92f8648f774430f3c785e6035862aa26be2fa3679848528f22989f8bb9dd08ed11337e2201ecac7f1358b'
